bf71033ee49bac2eeaf23f591df705fac9c7228e
Muhammed Munees <muneesmmm@gmail.com>
2021-05-16 16:41:30 +0530

    REST API added

--- src/bot.js
+++ src/bot.js
@@ -1,5 +1,5 @@
 require('dotenv').config();
-
+const fetch = require('node-fetch');
 const { Client } = require('discord.js');
 const PREFIX="*";
  
@@ -7,7 +7,7 @@ const client= new Client();
 client.on('ready', () => {
 console.log(`${client.user.username} has logged in`)
 });
-client.on('message', (message) => {
+client.on('message', async(message) => {
     console.log(`${message.author.tag} sent ${message.content}`)
     if (message.content === "1") {message.reply(`hi`)}
     if (message.content === "2") {message.reply(`hello`)}
@@ -19,6 +19,10 @@ client.on('message', (message) => {
     if (CMD_NAME === 'check')
     {message.channel.send('checking for avialability')}
     }
+    if (message.content === "5") {
+		const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
+		message.channel.send(file);
+	}
     
  });
 client.login(process.env.DISCORDJS_BOT_TOKEN); 
